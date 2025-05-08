b07d3c5349bf3a69ea5485712568f2682262ddf8
Andrew Li <liandrew620@gmail.com>
2025-05-08 18:41:29 -0500

    basic EBNF

---
+++ grammar/parse_generator.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+This program's job is to take in an EBNF grammar and spit out a parser for that grammar
+The parser that gets generated should be whitespace agnostic, so should replace all substrings of whitespaces with just 1 space. 
+
+The input EBNF should be all space separated. This is so I can use the whitespace to tokenize it. 
+
+I should probably formally define the EBNF grammar that this is able to parse. 
+*/
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    //first, need to parse the EBNF file
+
+    
+    //then, spit out code
+    
+    return 0;
+}
